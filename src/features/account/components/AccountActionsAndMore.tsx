604f680ac4d3d1abd5d7b9e740191b6241b2846a
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-10-11 10:13:10 +0100

    added logout

--- src/features/account/components/AccountActionsAndMore.tsx
+++ src/features/account/components/AccountActionsAndMore.tsx
@@ -1,8 +1,15 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useState } from "react";
+import { SignOut } from "src/components/layout/SignOut";
 import { formatToNaira } from "src/features/dashboard/utils";
 
 export const AccountActionsAndMore = () => {
+  const [openLogout, setOpenLogout] = useState(false);
   return (
+    <>
+     {/* Logout confirmation */}
+     <SignOut open={openLogout} handleClose={() => setOpenLogout(false)} />
+
     <div className="flex flex-col gap-4">
       <div className="border cursor-pointer group flex items-center text-accentSecondary gap-1 shadow-sm rounded-md px-4 py-4">
         <Icon icon="tdesign:add" className="text-3xl" />
@@ -31,14 +38,14 @@ export const AccountActionsAndMore = () => {
         <span className="text-sm group-hover:text-secondary">Contact Us</span>
       </div>
 
-      <div className="border cursor-pointer justify-between group flex items-center text-red-600  shadow-sm rounded-md px-4 py-4">
+      <div onClick={() => setOpenLogout(true)} className="border cursor-pointer justify-between group flex items-center text-red-600  shadow-sm rounded-md px-4 py-4">
         <div className="flex items-center gap-[10px]">
           <Icon icon="uiw:logout" className="text-2xl" />
           <span className="text-sm group-hover:text-secondary">Logout</span>
         </div>
-
         <Icon icon="iconamoon:arrow-right-2-thin" className="text-2xl text-accentSecondary"/>
       </div>
     </div>
+    </>
   );
 };
